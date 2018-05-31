035d9497486bf695603b0ec703ebcd1680c71bcd
Denis Chizh <denischizh@divandesign.ru>
2018-05-31 13:59:57 +0500

    Сделана проверка кода по eslint и выполнены правки по замечаниям в реквесте

--- js/main.js
+++ js/main.js
@@ -1,64 +1,45 @@
-// собрать все ссылки template в общий массив
-// нужно перебрать все темплейты и добавить их в массив
+'use strict';
 
-const ARROW_LEFT = 37;
-const ARROW_RIGHT = 39;
-
-const allTemplateScreensCollection = document.querySelectorAll("template");
-
-let allTemplateScreensArray = [];
-// циклом вставляем новые элементы массива
-for (let i = 0; i < allTemplateScreensCollection.length; i++) {
-  allTemplateScreensArray[i] = allTemplateScreensCollection[i];
-}
-
-
-// отрисовка функции, аргумент который будет порядковый номер темплейта, которая будет отрисовывать контент в main.central
-
-let renderedContent = document.querySelector('main.central');
-
-let currentPageNumber = 0;
-const renderTemplate = (pageNumber) => {
-//здесь происходит замена контента в main
-
-  //  1. берем нужный элемент темплейта
-  // логика для работы с переключениями страниц
-  pageNumber = pageNumber < 0 ? allTemplateScreensArray.length - 1 : pageNumber;
-  pageNumber = pageNumber >= allTemplateScreensArray.length ? 0 : pageNumber;
-  currentPageNumber = pageNumber;
-  let selectedTemplate = allTemplateScreensArray[currentPageNumber].innerHTML;
-  //создаем дом элемент
-  let container = document.createElement('div');
-  container.classList.add('inserted__content');
-  container.innerHTML = selectedTemplate;
-  renderedContent.innerHTML = '';
-  renderedContent.appendChild(container);
+const ARROW = {
+  LEFT: 37,
+  RIGHT: 39
 };
 
-renderTemplate(0);
+const templateCollectionScreens = document.querySelectorAll(`template`);
 
-// ? как решить это через replaceChild
+let templateScreens = Array.from(templateCollectionScreens);
 
-// логика события нажатия на кнопки
+const pageContainer = document.querySelector(`main.central`);
 
+let currentPageNumber = 0;
 
-document.addEventListener('keydown', function(evt) {
-  if (evt.keyCode == ARROW_LEFT) {
-  renderTemplate(currentPageNumber - 1);
-  }
-  else if (evt.keyCode == ARROW_RIGHT) {
+const replacePageContent = () => {
+  let selectedTemplate = templateScreens[currentPageNumber].cloneNode(true).content;
+  pageContainer.innerHTML = ``;
+  pageContainer.appendChild(selectedTemplate);
+};
+
+const renderTemplate = (pageNumber) => {
+  pageNumber = pageNumber < 0 ? templateScreens.length - 1 : pageNumber;
+  pageNumber = pageNumber >= templateScreens.length ? 0 : pageNumber;
+  currentPageNumber = pageNumber;
+  replacePageContent();
+};
+
+renderTemplate(currentPageNumber);
+
+document.addEventListener(`keydown`, function (evt) {
+  if (evt.keyCode === ARROW.LEFT) {
+    renderTemplate(currentPageNumber - 1);
+  } else if (evt.keyCode === ARROW.RIGHT) {
     renderTemplate(currentPageNumber + 1);
   }
 });
 
-// отрисовка стрелок и логика нажатия на них
-
-//вставляем стрелки
-
-const arrowsNavigationHTML = `
-<div class="arrows__wrap">
+const bodyTag = document.querySelector(`body`);
+bodyTag.insertAdjacentHTML(`beforeEnd`, `<div class="arrows__wrap">
   <style>
-.arrows__wrap {
+  .arrows__wrap {
   position: absolute;
   top: 95px;
   left: 50%;
@@ -71,25 +52,14 @@ const arrowsNavigationHTML = `
 }
 </style>
 <button class="arrows__btn"><-</button>
-<button class="arrows__btn">-></button>
-</div>
-`;
+  <button class="arrows__btn">-></button>
+  </div>`);
 
-// сейчас мы получили результат в виде строки, а нам надо в виде дом-узла
-const arrowsBlock = document.createElement('div');
-arrowsBlock.innerHTML = arrowsNavigationHTML;
-
-const bodyTag = document.querySelector('body');
-bodyTag.appendChild(arrowsBlock);
-
-// добавим событие клика на добавленные стрелки и используем механизм всплытия событий, добавив его на контейнер с кнопками
-
-document.querySelector('.arrows__wrap').addEventListener('click', function(evt) {
-  console.log(evt.target);
-  if (evt.target == document.querySelector('.arrows__btn:first-of-type')) {
+const arrowWrapper = document.querySelector(`.arrows__wrap`);
+arrowWrapper.addEventListener(`click`, function (evt) {
+  if (evt.target === document.querySelector(`.arrows__btn:first-of-type`)) {
     renderTemplate(currentPageNumber - 1);
-  }
-  else if (evt.target == document.querySelector('.arrows__btn:last-of-type')) {
+  } else if (evt.target === document.querySelector(`.arrows__btn:last-of-type`)) {
     renderTemplate(currentPageNumber + 1);
   }
 });
