6331d2616e97401c3ec1aa315217d0467933790f
Denis Chizh <denischizh@divandesign.ru>
2018-06-05 17:40:08 +0500

    начало работы над тестированием функции расчета очков после окончания игры

--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -7,3 +7,36 @@ describe(`Array`, () => {
     });
   });
 });
+
+//здесь нужно описывать новые тесты для новых функций, а потом TDD(написание функций через тестирование)
+
+const scoreAfterGame = (userAnswers, remainingLifes) => {
+  let userPoints = 0;
+//  нужно обработать ответы пользователя. каждый правильный +100 очков, быстрый ответ еще +50, медленный -50.
+  if (userAnswers.length < 10) {
+    return -1;
+  } else {
+    for (let i = 0; i < userAnswers.length; i++) {
+      if (userAnswers[i] ) {
+        userPoints = userPoints + 100;
+      }
+    }
+  }
+  userPoints = userPoints + (remainingLifes * 50);
+//за каждую оставшуюся жизнь +50
+
+//  на выходе функция получает количество набранных очков.
+  return userPoints;
+};
+
+
+//начало тестирования
+
+describe(`Score user points after game`, () => {
+
+  it(`should update score`, () => {
+    assert.equal(scoreAfterGame(['first answer', 'second answer'], 10), -1);
+    assert.equal(scoreAfterGame(['first answer', 'second', '3', '4', '5', '6', '7', '8', '9', '10', '11'], 10), 1600);
+  });
+
+});
