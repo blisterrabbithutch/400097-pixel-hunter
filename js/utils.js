0372cf67efc0407b3c8d516d10582eef16f78b05
Denis Chizh <denischizh@divandesign.ru>
2018-06-25 23:31:07 +0500

    удалены лишние утилитарные функции

--- js/utils.js
+++ js/utils.js
@@ -32,68 +32,6 @@ const getElementFromTemplate = (template) => {
   return pageElement.firstElementChild;
 };
 
-const handleResultOfOneCardLevel = (data, firstCardInputsValue) => {
-  let answerIsSolved;
-  if (firstCardInputsValue === data.cards[0].rightAnswer) {
-    answerIsSolved = true;
-  } else {
-    userState.lives = userState.lives - 1;
-    answerIsSolved = false;
-  }
-  let answerOnCard = {
-    time: AnswerTime.NORMAL,
-    solved: answerIsSolved
-  };
-  userState.answers.push(answerOnCard);
-  return answerOnCard;
-};
-
-const handleResultOfTwoCardsLevel = (currentLevel, firstCardInputsValue, secondCardInputsValue) => {
-  let answerIsSolved;
-  if (firstCardInputsValue === currentLevel.cards[0].rightAnswer && secondCardInputsValue === currentLevel.cards[1].rightAnswer) {
-    answerIsSolved = true;
-  } else {
-    userState.lives = userState.lives - 1;
-    answerIsSolved = false;
-  }
-  let answerOnCard = {
-    time: AnswerTime.NORMAL,
-    solved: answerIsSolved
-  };
-  userState.answers.push(answerOnCard);
-  return answerOnCard;
-};
-
-const findDifferentCard = (firstCard, secondCard, thirdCard) => {
-  const firstCardEl = document.querySelector(`.game__option:first-child`);
-  const secondCardEl = document.querySelector(`.game__option:nth-child(2)`);
-  const thirdCardEl = document.querySelector(`.game__option:nth-child(3)`);
-  if (firstCard !== secondCard && firstCard !== thirdCard) {
-    return firstCardEl;
-  } else if (secondCard !== firstCard && secondCard !== thirdCard) {
-    return secondCardEl;
-  } else if (thirdCard !== firstCard && thirdCard !== firstCard) {
-    return thirdCardEl;
-  }
-  throw new Error(`Incorrect type of parameters. (need paint or photo string)`);
-};
-
-const handleResultOfThreeCardsLevel = (evt, firstCardAnswer, secondCardAnswer, thirdCardAnswer) => {
-  let answerIsSolved;
-  if (evt.target === findDifferentCard(firstCardAnswer, secondCardAnswer, thirdCardAnswer)) {
-    answerIsSolved = true;
-  } else {
-    userState.lives = userState.lives - 1;
-    answerIsSolved = false;
-  }
-  let answerOnCard = {
-    time: AnswerTime.NORMAL,
-    solved: answerIsSolved
-  };
-  userState.answers.push(answerOnCard);
-  return answerOnCard;
-};
-
 const getScore = (userResult, remainingLifes) => {
   let userPoints = 0;
   if (userResult.length < 10) {
@@ -171,4 +109,4 @@ const getLevelProgressBar = (userAnswers) => {
   return userLevels;
 };
 
-export {getElementFromTemplate, createTimer, createUserdata, userState, handleResultOfTwoCardsLevel, handleResultOfOneCardLevel, handleResultOfThreeCardsLevel, findDifferentCard, getLevelProgressBar, getScore, getFastAnswersValue, getSlowAnswersValue, getCompletedLevelsValue};
+export {getElementFromTemplate, createTimer, createUserdata, userState, getLevelProgressBar, getScore, getFastAnswersValue, getSlowAnswersValue, getCompletedLevelsValue};
